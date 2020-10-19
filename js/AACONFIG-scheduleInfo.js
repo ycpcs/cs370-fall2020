d208725868f49fc9894d95827a0fde52d538d6c4
David Babcock <dbabcock@ycp.edu>
2020-10-19 16:30:40 -0400

    fix final exam time
    fix final exam time

--- js/AACONFIG-scheduleInfo.js
+++ js/AACONFIG-scheduleInfo.js
@@ -135,7 +135,7 @@ courseInfo.classPeriods = [
 // The following is for the college-scheduled final exam.
 // It is not used if final is on last day of class"
 courseInfo.finalExamDates = [
-		new FinalExamDay("102", new Date("12/03/2020 10:15:00")),
+		new FinalExamDay("102", new Date("12/03/2020 12:45:00")),
 		new FinalExamDay("101", new Date("12/05/2020 10:15:00")),
 //		new FinalExamDay("103", new Date("12/13/2019 12:45:00")),
 //		new FinalExamDay("104", new Date("12/13/2019 15:00:00"))
